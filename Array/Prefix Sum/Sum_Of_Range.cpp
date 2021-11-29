ae7488da86ff4e3759ea37fd766bf87e815cbc78
Jash Nimje <40354857+jashnimje@users.noreply.github.com>
2021-11-29 23:39:23 +0530

    DSA - Prefix Sum

---
+++ Array/Prefix Sum/Sum_Of_Range.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define rep(i, n) for (i = 0; i < n; i++)
+#define ll long long
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%lld\n", x)
+#define ps(s) printf("%s\n", s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+const int mod = 1'000'000'007;
+const int N = 3e5, M = N;
+//=======================
+
+void solve()
+{
+    int i, n, q, l, r, sum = 0;
+    cin >> n;
+    int arr[n];
+    rep(i, n)
+    {
+        cin >> arr[i];
+    }
+    cin >> q;
+    int prefix[n];
+    clr(prefix);
+    rep(i, n)
+    {
+        sum += arr[i];
+        prefix[i] = sum;
+    }
+    while (q--)
+    {
+        cin >> l >> r;
+        int ans = 0;
+        if (l == 0)
+        {
+            ans = prefix[r];
+        }
+        else
+        {
+            ans = prefix[r] - prefix[l - 1];
+        }
+        cout << ans << endl;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+    solve();
+
+    return 0;
+}
